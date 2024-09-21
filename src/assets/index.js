c45da254589e38ddb2ceb7ff2a1485c15675e68b
argus-li <mobilicorpus@icloud.com>
2024-09-21 18:19:57 +0800

    Add demo image

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import AppleWebp from "./apple.webp";
+import AxeWebp from "./axe.webp";
+import BookWebp from "./book.webp";
+import bowWebp from "./bow.webp";
+import CarrotWebp from "./carrot.webp";
+import CrossbowWebp from "./crossbow.webp";
+import DiamondSwordPng from "./diamond_sword.png";
+import DragonEggWebp from "./dragon_egg.webp";
+import EggWebp from "./egg.webp";
+import FishingRodWebp from "./fishing_rod.webp";
+import GoldenSwordWebp from "./golden_sword.webp";
+import HoneyBottleWebp from "./honey_bottle.webp";
+import PickaxeWebp from "./pickaxe.webp";
+import ShovelWebp from "./shovel.webp";
+import SpectralArrowWebp from "./spectral_arrow.webp";
+
+export {
+	AppleWebp,
+	AxeWebp,
+	BookWebp,
+	bowWebp,
+	CarrotWebp,
+	CrossbowWebp,
+	DiamondSwordPng,
+	DragonEggWebp,
+	EggWebp,
+	FishingRodWebp,
+	GoldenSwordWebp,
+	HoneyBottleWebp,
+	PickaxeWebp,
+	ShovelWebp,
+	SpectralArrowWebp,
+};
